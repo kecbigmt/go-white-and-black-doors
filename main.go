59bb3b17caa39cefd6bc4fdb3acdfe8395fdb4d3
Kecy <keishiro.oym@gmail.com>
2017-08-26 22:51:16 +0900

    modify main.go

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 import (
-  "github.com/kecbigmt/go-white-and-black-doors/automata/oldLulu_047"
+  "github.com/kecbigmt/go-white-and-black-doors/automata/oldLulu_001"
   "fmt"
 )
 
 func main(){
-  bs := oldLulu_047.TestPatterns
+  bs := oldLulu_001.TestPatterns
   for _, b := range bs{
     fmt.Println("Try:", b)
-    err := oldLulu_047.Validate(b)
+    err := oldLulu_001.Validate(b)
     if err != nil{
       fmt.Println("-> Invalid:",err)
     }else{
